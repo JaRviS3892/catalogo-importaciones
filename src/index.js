d62151ebbfe44c4a72639007d1c9367bc00579aa
javier andres hernandez reveco <javier.hernandez84897@edu.ipchile.cl>
2025-08-29 04:36:31 -0400

    chore: remove unused packages

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CartProvider } from "./context/CartContext";
 
-const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <CartProvider>
-                    <App />
-                </CartProvider>
-            </QueryClientProvider>
-        </BrowserRouter>
+        <CartProvider>
+            <App />
+        </CartProvider>
     </React.StrictMode>
 );
